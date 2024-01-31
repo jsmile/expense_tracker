9fffe557aea899d0f90e4a0ae8a3e649b2e5b14c
jsmile <jsmile@naver.com>
2024-01-31 20:18:48 +0900

    MediaQuery.of(context).size 작업중.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:ansicolor/ansicolor.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 import 'src/app.dart';
 
 void main() {
   ansiColorDisabled = false;
   WidgetsFlutterBinding.ensureInitialized();
-  // 선언된 것들만 허용함.
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp, // 세로 보기만 허용.
-  ]).then((value) {
-    runApp(
-      const App(),
-    );
-  });
+  // // 선언된 것들만 허용함.
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp, // 세로 보기만 허용.
+  // ]).then((value) {
+  runApp(
+    const App(),
+  );
+  // });
 }
