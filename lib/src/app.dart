ef7134f36d9bbee0ab93429f51191fbb0f15d0d6
jsmile <jsmile@naver.com>
2024-01-31 15:58:25 +0900

    Theme - dark mode

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,6 +6,10 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -52,6 +56,26 @@ class App extends StatelessWidget {
         //           ),
         //     ),
       ), // 최신 버전에서는 기본값이 true
+      darkTheme: ThemeData.dark().copyWith(
+        brightness: Brightness.dark, // 밝기 등을 어두운 색에 알맞게 조정
+        colorScheme: kDarkColorScheme,
+        // UI( Dismissible 의 background ) 에서 사용되고 있는 Theme 이므로
+        // dartk 에서도 재정의 필요
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer, // card 배경색
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 5.0,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+      // themeMode: ThemeMode.system,
       home: const Expenses(),
     );
   }
